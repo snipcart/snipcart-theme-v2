6c6dd1c079a58ab85a8dd8bc61176872030ff596
Bradley Sepos <bradley@bradleysepos.com>
2018-11-19 06:24:57 -0500

    Modernize npm dependencies and gulpfiles.

--- Gulpfile.coffee
+++ Gulpfile.coffee
@@ -1,14 +1,17 @@
 gulp = require 'gulp'
+FwdRef = require 'undertaker-forward-reference'
 es = require 'event-stream'
 sass = require 'gulp-sass'
 concat = require 'gulp-concat'
-minifyCss = require 'gulp-minify-css'
+cleanCSS = require 'gulp-clean-css'
 rename = require 'gulp-rename'
 autoprefixer = require 'gulp-autoprefixer'
 postcss = require 'gulp-postcss'
 mqpacker = require 'css-mqpacker'
 browserSync = require 'browser-sync'
 argv = require('yargs').argv
+
+gulp.registry(FwdRef())
 
 themesDir = 'themes'
 workingDir = 'themes/base'
@@ -29,28 +32,24 @@ getDistDir = (output)->
     dir += "/#{output}"
   return dir
 
-gulp.task 'sass', ->
-  gulp.src sources.sass
+sassTask = () ->
+  return gulp.src sources.sass
     .pipe(sass().on('error', sass.logError))
     .pipe autoprefixer(cascade: false, browsers: ['> 0.25%'])
     .pipe gulp.dest(workingDir)
 
-gulp.task 'min', ['sass'], ->
-   gulp.src sources.compiled
+minTask = () ->
+  return gulp.src sources.compiled
     .pipe(postcss([mqpacker]))
-    .pipe(minifyCss())
+    .pipe(cleanCSS())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(workingDir))
 
-  gulp.src(sources.compiled)
-    .pipe(minifyCss())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(workingDir))
+watchTask = gulp.series (done) ->
+  gulp.watch([watch.sass]).on 'change', sassTask
+  done()
 
-gulp.task 'watch', ->
-  gulp.watch [watch.sass], ['sass']
-
-gulp.task 'sync', ['dev'], ->
+syncTask = gulp.series (done) ->
   proxy = argv.proxy ? 'snipcart.client/'
   sync = browserSync.create proxy
   sync.init
@@ -58,11 +57,19 @@ gulp.task 'sync', ['dev'], ->
     proxy: proxy
     serveStatic: ['.']
     ui:
-        port: 3007
+      port: 3007
   gulp.watch(sources.compiled).on 'change', sync.reload
+  done()
 
-gulp.task 'dev', ['default', 'watch']
-gulp.task 'default', ['sass']
-gulp.task 'deploy', ['min'], ->
-  gulp.src(["#{themesDir}/**/*", '!**/*.scss', '!**/sass', '!**/styles.css'])
+deployTask = () ->
+  return gulp.src(["#{themesDir}/**/*", '!**/*.scss', '!**/sass', '!**/styles.css'])
     .pipe(gulp.dest(getDistDir()))
+
+gulp.task 'sass',    gulp.series(sassTask)
+gulp.task 'min',     gulp.series('sass', minTask)
+gulp.task 'watch',   gulp.series(watchTask)
+gulp.task 'dev',     gulp.series('default', 'watch')
+gulp.task 'sync',    gulp.series('dev', syncTask)
+gulp.task 'deploy',  gulp.series('min', deployTask)
+
+gulp.task 'default', gulp.series('sass')
--- wip.Gulpfile.js
+++ wip.Gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
+var FwdRef = require('undertaker-forward-reference');
 var es = require('event-stream');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var argv = require('yargs').option('version', {type: 'string'}).argv;
@@ -11,12 +12,14 @@ var mqpacker = require('css-mqpacker');
 var browserSync = require('browser-sync');
 var argv = require('yargs').argv
 
+gulp.registry(FwdRef());
+
 var themesDir = 'themes';
 var workingDir = 'themes/base';
 
 var sources = {
-    sass: workingDir + '/sass/snipcart.scss',
-    compiled: workingDir + '/snipcart.css'
+  sass: workingDir + '/sass/snipcart.scss',
+  compiled: workingDir + '/snipcart.css'
 };
 
 var watch = {
@@ -24,57 +27,64 @@ var watch = {
 };
 
 var getDistDir = function (output) {
-    var dir = './dist/themes';
-
-    if (argv.version) {
-        dir = "./dist/themes/" + argv.version;
-    }
-
-    if (output) {
-        dir += '/' + output;
-    }
-
-    return dir;
+  var dir = './dist/themes';
+  if (argv.version) {
+    dir = "./dist/themes/" + argv.version;
+  }
+  if (output) {
+    dir += '/' + output;
+  }
+  return dir;
 };
 
-gulp.task('sass', function() {
-    gulp.src(sources.sass)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({cascade: false, browsers: ['> 0.25%']}))
-        .pipe(gulp.dest(workingDir));
+var sassTask = function() {
+  return gulp.src(sources.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false, browsers: ['> 0.25%']}))
+    .pipe(gulp.dest(workingDir));
+};
+
+var minTask = function() {
+  return gulp.src(sources.compiled)
+    .pipe(postcss([mqpacker]))
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(workingDir));
+};
+
+var watchTask = gulp.series(function(done) {
+  gulp.watch([watch.sass]).on('change', function(event) {
+    sassTask;
+  });
+  done();
 });
 
-gulp.task('min', ['sass'], function() {
-    gulp.src(sources.compiled)
-        .pipe(postcss([mqpacker]))
-        .pipe(minifyCss())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(workingDir));
+var syncTask = gulp.series(function(done) {
+  var proxy = argv.proxy ? argv.proxy : 'snipcart.client/';
+  var sync = browserSync.create(proxy);
+  sync.init({
+    port: 3006,
+    proxy: proxy,
+    cors: true,
+    serveStatic: ['.'],
+    ui: {
+      port: 3007
+    }
+  });
+  gulp.watch(sources.compiled).on('change', sync.reload);
+  done();
 });
 
-gulp.task('watch', function() {
-    gulp.watch([watch.sass], ['sass']);
-});
+var deployTask = function() {
+  return gulp.src([themesDir + "/**/*", '!**/*.scss', '!**/sass', '!**/styles.css'])
+    .pipe(gulp.dest(getDistDir()));
+};
 
-gulp.task('sync', ['dev'], function() {
-    var proxy = argv.proxy ? argv.proxy : 'snipcart.client/';
-    var sync = browserSync.create(proxy);
-    sync.init({
-        port: 3006,
-        proxy: proxy,
-        cors: true,
-        serveStatic: ['.'],
-        ui: {
-            port: 3007
-        }
-    });
+gulp.task('sass',    gulp.series(sassTask));
+gulp.task('min',     gulp.series('sass', minTask));
+gulp.task('watch',   gulp.series(watchTask));
+gulp.task('dev',     gulp.series('default', 'watch'));
+gulp.task('sync',    gulp.series('dev', syncTask));
+gulp.task('deploy',  gulp.series('min', deployTask));
 
-    gulp.watch(sources.compiled).on('change', sync.reload);
-});
-
-gulp.task('dev', ['default', 'watch']);
-gulp.task('default', ['sass']);
-gulp.task('deploy', ['min'], function() {
-    gulp.src([themesDir + "/**/*", '!**/*.scss', '!**/sass', '!**/styles.css'])
-        .pipe(gulp.dest(getDistDir()))
-});
+gulp.task('default', gulp.series('sass'));
